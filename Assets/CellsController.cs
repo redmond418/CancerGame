b8909a04ea2c4e86d7fa3a06b9887463f585871e
redmond418 <redmond418@yahoo.co.jp>
2022-06-14 23:33:27 +0900

    よしなに動く

--- Assets/CellsController.cs
+++ Assets/CellsController.cs
@@ -16,13 +16,13 @@ public class CellsController : MonoBehaviour
     SpriteRenderer[,] cells;
     int[,] nextCells;
 #if UNITY_EDITOR
-    readonly Color gizmoColor = new Color(1, 0, 0, 0.3f);
+    readonly Color gizmoColor = new Color(0, 1f, 0.4f, 0.3f);
     bool[] gizmoToCell;
     /*[System.NonSerialized] public */bool[,] gizmo2DArray;
 
     private void OnDrawGizmos()
     {
-        if(gizmoToCell == null || !gizmoToCell.SequenceEqual(placement.cellsArray))
+        if(gizmo2DArray == null || !gizmoToCell.SequenceEqual(placement.cellsArray))
         {
             gizmoToCell = new bool[placement.cellsArray.Length];
             placement.cellsArray.CopyTo(gizmoToCell,0);
@@ -33,14 +33,14 @@ public class CellsController : MonoBehaviour
         if(gizmo2DArray != null)
         {
             Gizmos.color = gizmoColor;
-            Gizmos.DrawWireCube(Vector2.left * 0.5f + Vector2.up * 0.5f, Vector2.one * gizmo2DArray.GetLength(0));
+            Gizmos.DrawWireCube(Vector2.zero, Vector2.one * gizmo2DArray.GetLength(0));
             if (!EditorApplication.isPlaying)
             {
                 for (int i = 0; i < gizmo2DArray.GetLength(1); i++)
                 {
                     for (int j = 0; j < gizmo2DArray.GetLength(0); j++)
                     {
-                        if (gizmo2DArray[j, i]) Gizmos.DrawCube(new(j - gizmo2DArray.GetLength(1) * 0.5f, -i + gizmo2DArray.GetLength(0) * 0.5f), Vector2.one);
+                        if (gizmo2DArray[j, i]) Gizmos.DrawCube(new(j - gizmo2DArray.GetLength(1) * 0.5f + 0.5f, -i + gizmo2DArray.GetLength(0) * 0.5f - 0.5f), Vector2.one);
                     }
                 }
             }
@@ -59,7 +59,7 @@ public class CellsController : MonoBehaviour
             for (int j = 0; j < cells.GetLength(0); j++)
             {
                 if(cellsBool[j,i]) cells[j, i] = Instantiate<SpriteRenderer>(cellPrefab,
-                     new(j - cells.GetLength(1) * 0.5f, -i + cells.GetLength(0) * 0.5f), Quaternion.identity);
+                     new(j - cells.GetLength(1) * 0.5f + 0.5f, -i + cells.GetLength(0) * 0.5f - 0.5f), Quaternion.identity);
             }
         }
         //StartCoroutine(SetOtherSells());
@@ -84,79 +84,105 @@ public class CellsController : MonoBehaviour
             {
                 if (nextCells[j, i] > 0)
                 {
-                    if (cellsInPool.Count > 0)
+                    if (cellsInPool.Count(n => !n.gameObject.activeSelf) > 0)
                     {
-                        cells[j, i] = cellsInPool[0];
-                        cells[j, i].transform.position = new(j - cells.GetLength(1) * 0.5f, -i + cells.GetLength(0) * 0.5f);
+                        cells[j, i] = cellsInPool.First(n => !n.gameObject.activeSelf);
+                        cells[j, i].transform.position = new(j - cells.GetLength(1) * 0.5f + 0.5f, -i + cells.GetLength(0) * 0.5f - 0.5f);
                         Material cellM = cells[j, i].material;
                         int surroundingsMode = nextCells[j, i] - 1;
-                        cellM.SetInt("_Right", surroundingsMode % 2 == 1 ? 1 : 0);
-                        cellM.SetInt("_Left", surroundingsMode % 4 >= 2 ? 1 : 0);
-                        cellM.SetInt("_Up", surroundingsMode % 8 >= 4 ? 1 : 0);
-                        cellM.SetInt("_Down", surroundingsMode >= 8 ? 1 : 0);
-                        cellM.SetFloat("_Value", 1);
+                        if(surroundingsMode == 0)
+                        {
+                            cellM.SetInt("_Right", 1);
+                            cellM.SetInt("_Left", 1);
+                            cellM.SetInt("_Up", 1);
+                            cellM.SetInt("_Down", 1);
+                            cellM.SetInt("_Reverse", 1);
+                            cellM.SetFloat("_Value", 0);
+                            cellM.DOFloat(1, "_Value", 0.2f)
+                                .SetEase(Ease.InOutQuad);
+                        }
+                        else
+                        {
+                            cellM.SetInt("_Right", surroundingsMode % 2 == 1 ? 1 : 0);
+                            cellM.SetInt("_Left", surroundingsMode % 4 >= 2 ? 1 : 0);
+                            cellM.SetInt("_Up", surroundingsMode % 8 >= 4 ? 1 : 0);
+                            cellM.SetInt("_Down", surroundingsMode >= 8 ? 1 : 0);
+                            cellM.SetInt("_Reverse", 0);
+                            cellM.SetFloat("_Value", 1);
+                            cellM.DOFloat(0, "_Value", 0.2f)
+                                .SetEase(Ease.InOutQuad);
+                        }
                         cells[j, i].gameObject.SetActive(true);
-                        cellM.DOFloat(0, "_Value", 0.2f);
-                        cellsInPool.RemoveAt(0);
+                        cellsInPool.Remove(cells[j, i]);
                     }
                     else
                     {
                         cells[j, i] = Instantiate<SpriteRenderer>(cellPrefab,
-                            new(j - cells.GetLength(1) * 0.5f, -i + cells.GetLength(0) * 0.5f), Quaternion.identity);
+                            new(j - cells.GetLength(1) * 0.5f + 0.5f, -i + cells.GetLength(0) * 0.5f - 0.5f), Quaternion.identity);
                         Material cellM = cells[j, i].material;
                         int surroundingsMode = nextCells[j, i] - 1;
-                        cellM.SetInt("_Right", surroundingsMode % 2 == 1 ? 1 : 0);
-                        cellM.SetInt("_Left", surroundingsMode % 4 >= 2 ? 1 : 0);
-                        cellM.SetInt("_Up", surroundingsMode % 8 >= 4 ? 1 : 0);
-                        cellM.SetInt("_Down", surroundingsMode >= 8 ? 1 : 0);
-                        cellM.SetFloat("_Value", 1);
-                        cellM.DOFloat(0, "_Value", 0.2f);
+                        if (surroundingsMode == 0)
+                        {
+                            cellM.SetInt("_Right", 1);
+                            cellM.SetInt("_Left", 1);
+                            cellM.SetInt("_Up", 1);
+                            cellM.SetInt("_Down", 1);
+                            cellM.SetInt("_Reverse", 1);
+                            cellM.SetFloat("_Value", 0);
+                            cellM.DOFloat(1, "_Value", 0.2f)
+                                .SetEase(Ease.InOutQuad);
+                        }
+                        else
+                        {
+                            cellM.SetInt("_Right", surroundingsMode % 2 == 1 ? 1 : 0);
+                            cellM.SetInt("_Left", surroundingsMode % 4 >= 2 ? 1 : 0);
+                            cellM.SetInt("_Up", surroundingsMode % 8 >= 4 ? 1 : 0);
+                            cellM.SetInt("_Down", surroundingsMode >= 8 ? 1 : 0);
+                            cellM.SetInt("_Reverse", 0);
+                            cellM.SetFloat("_Value", 1);
+                            cellM.DOFloat(0, "_Value", 0.2f)
+                                .SetEase(Ease.InOutQuad);
+                        }
                     }
                     nextCells[j, i] = 0;
                 }
                 else if (nextCells[j, i] < 0)
                 {
                     cellsInPool.Add(cells[j, i]);
-                    cells[j, i].gameObject.SetActive(false);
+                    Material cellM = cells[j, i].material;
+                    int surroundingsMode = -nextCells[j, i] - 1;
+                    if (surroundingsMode == 0)
+                    {
+                        cellM.SetInt("_Right", 1);
+                        cellM.SetInt("_Left", 1);
+                        cellM.SetInt("_Up", 1);
+                        cellM.SetInt("_Down", 1);
+                        cellM.SetInt("_Reverse", 1);
+                        cellM.SetFloat("_Value", 1);
+                        GameObject cellObj = cells[j, i].gameObject;
+                        cellM.DOFloat(0, "_Value", 0.2f)
+                                .SetEase(Ease.InOutQuad)
+                                .OnComplete(() => cellObj.SetActive(false));
+                    }
+                    else
+                    {
+                        cellM.SetInt("_Right", surroundingsMode % 2 == 1 ? 1 : 0);
+                        cellM.SetInt("_Left", surroundingsMode % 4 >= 2 ? 1 : 0);
+                        cellM.SetInt("_Up", surroundingsMode % 8 >= 4 ? 1 : 0);
+                        cellM.SetInt("_Down", surroundingsMode >= 8 ? 1 : 0);
+                        cellM.SetInt("_Reverse", 0);
+                        cellM.SetFloat("_Value", 0);
+                        GameObject cellObj = cells[j, i].gameObject;
+                        cellM.DOFloat(1, "_Value", 0.2f)
+                                .SetEase(Ease.InOutQuad)
+                                .OnComplete(() => cellObj.SetActive(false));
+                    }
                     cells[j, i] = null;
                     nextCells[j, i] = 0;
                 }
             }
         }
         StartCoroutine(GetNextCells());
-    }
-
-    Vector2Int CellCheck(Vector2Int pos)
-    {
-        Vector2Int min = -Vector2Int.one;
-        Vector2Int max = Vector2Int.one;
-        if (pos.x <= 0) min.x = 0;
-        else if (pos.x >= cells.GetLength(0) - 1) max.x = 0;
-        if (pos.y <= 0) min.y = 0;
-        else if (pos.y >= cells.GetLength(1) - 1) max.y = 0;
-        int num = 0;
-        int mode = 0;
-        for (int i = min.y; i <= max.y; i++)
-        {
-            for (int j = min.x; j <= max.x; j++)
-            {
-                if ((j != 0 || i != 0) && cells[pos.x + j,pos.y + i] && cells[pos.x + j, pos.y + i].gameObject.activeSelf)
-                {
-                    num++;
-                    if(i == 0)
-                    {
-                        if (j == 1) mode += 1;
-                        else mode += 2;
-                    }
-                    if(j == 0)
-                    {
-                        if (i == 1) mode += 4;
-                        else mode += 8;
-                    }
-                }
-            }
-        }
-        return new(num, mode);
     }
 
     Vector2Int CellCheck(int posx,int posy)
@@ -183,13 +209,45 @@ public class CellsController : MonoBehaviour
                     }
                     if (j == 0)
                     {
-                        if (i == 1) mode += 4;
+                        if (i == -1) mode += 4;
                         else mode += 8;
                     }
                 }
             }
         }
         return new(num, mode);
+    }
+
+    int SecondCellCheck(int posx, int posy)
+    {
+        Vector2Int min = -Vector2Int.one;
+        Vector2Int max = Vector2Int.one;
+        if (posx <= 0) min.x = 0;
+        else if (posx >= cells.GetLength(0) - 1) max.x = 0;
+        if (posy <= 0) min.y = 0;
+        else if (posy >= cells.GetLength(1) - 1) max.y = 0;
+        int mode = 0;
+        for (int i = min.y; i <= max.y; i++)
+        {
+            for (int j = min.x; j <= max.x; j++)
+            {
+                if ((j != 0 || i != 0) && ((cells[posx + j, posy + i] && cells[posx + j, posy + i].gameObject.activeSelf) || 
+                    nextCells[posx + j,posy + i] >= 1))
+                {
+                    if (i == 0)
+                    {
+                        if (j == 1) mode += 1;
+                        else mode += 2;
+                    }
+                    if (j == 0)
+                    {
+                        if (i == -1) mode += 4;
+                        else mode += 8;
+                    }
+                }
+            }
+        }
+        return mode;
     }
 
     IEnumerator GetNextCells()
@@ -211,6 +269,17 @@ public class CellsController : MonoBehaviour
                     increaseCellsCount++;
                     nextCells[j, i] = info.y + 1;
                     if(cellsInPool.Count < increaseCellsCount) cellsInPool.Add(Instantiate<SpriteRenderer>(cellPrefab));
+                }
+            }
+        }
+        for (int i = 0; i < cells.GetLength(1); i++)
+        {
+            for (int j = 0; j < cells.GetLength(0); j++)
+            {
+                int surround = SecondCellCheck(j, i);
+                if (nextCells[j, i] <= -1)
+                {
+                    nextCells[j, i] = -surround - 1;
                 }
             }
         }
